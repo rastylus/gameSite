641aebce3d23f98f96623b88eb49396e081aafc1
rastylus <ryan.p.riggs@gmail.com>
2024-02-05 07:44:27 -0600

    color change buttons and opacoty slider

--- src/Paths/Home.jsx
+++ src/Paths/Home.jsx
@@ -1,24 +1,63 @@
-import { useState } from "react";
-import { BrowserRouter, Routes, Route , Link} from "react-router-dom";
-
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 function Home() {
   const [count, setCount] = useState(0);
+  const [color, setColor] = useState("purple");
+  const [opacity, setOpacity] = useState(1);
+  const [num, setNum] = useState(0);
+
+  const randomNumberInRange = (min, max) => {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  };
+
+  const handleClick = () => {
+    setNum(randomNumberInRange(1, 20));
+  };
+
+  const changeColor = () => {
+    setColor((prevColor) => (prevColor === "black" ? "white" : "black"));
+  };
+
+  const changeOpacity = (e) => {
+    setOpacity(e.target.value);
+  };
+
+  const backgroundColors = count % 2 === 0 ? "red" : "blue";
 
   return (
-    <>
-      <div></div>
+    <div
+      style={{
+        background: backgroundColors,
+        opacity: opacity,
+        color: color,
+        padding: "100px",
+      }}
+    >
       <div className="card">
+        <h2>Number is: {num}</h2>
+        <button onClick={handleClick}>Click Me Generate</button>
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <p>HELLO</p>
-        <p><Link to='/test'>test</Link></p>
-
-        <input type="range"></input>
+        <p>
+          <Link to="/test">test</Link>
+        </p>
+        <button onClick={changeColor}>Change Color</button>
+        <div>
+          <label htmlFor="opacityRange">Change Opacity:</label>
+          <input
+            type="range"
+            id="opacityRange"
+            min="0.1"
+            max="1"
+            step="0.1"
+            value={opacity}
+            onChange={changeOpacity}
+          ></input>
+        </div>
       </div>
-
-    </>
+    </div>
   );
 }
 

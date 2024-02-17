21d74e6b1e5a27503082dc6d7c5b9fa0558ef980
rastylus <ryan.p.riggs@gmail.com>
2024-02-17 16:00:14 -0600

    fixed error

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,16 @@
 import { useState } from "react";
-import { BrowserRouter, Routes, Route , Link} from "react-router-dom";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import "./App.css";
 import Test from "./pages/Test";
 import Home from "./pages/Home";
-// import Home from "./Pages/Home";
 import PlayPage from "./pages/PlayPage";
 
 function App() {
-
   return (
     <>
       <BrowserRouter>
         <Routes>
-        <Route index element={<Home />} />
+          <Route index element={<Home />} />
           <Route path="test" element={<Test />} />
           <Route path="/play" element={<PlayPage />} />
         </Routes>
--- src/Pages/Test.jsx
+++ src/Pages/Test.jsx
@@ -1,16 +1,16 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import Home from "./Home";
-import { Tone } from "tone/build/esm/core/Tone";
-
 
 function Test() {
-
-
   return (
     <>
       <p>test is here</p>
-      <p><Link to='/'>home</Link></p>
+      <p>
+        <Link to="/">home</Link>
+        <br></br>
+        <br></br>
+        WHO'S WHOSE BIH
+      </p>
     </>
   );
 }
